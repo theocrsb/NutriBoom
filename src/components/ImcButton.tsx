00471c18600792cdc31c8dbb4e74e2aaa9d20ef1
OZIER Ludovic (SNCF VOYAGEURS / TECHNICENTRE DE PARIS NORD / EQUIPE EXAMENS JOUR) <9001074b@commun.ad.sncf.fr>
2022-12-05 21:47:48 +0100

    ajout du bouton calcule ton imc et un peu de style sur welcome

---
+++ src/components/ImcButton.tsx
@@ -0,0 +1,8 @@
+const ImcButton =()=>{
+    return(
+        <div>
+            <button type="button" className="btn inscription">Calcule ton IMC</button>
+        </div>
+    )
+}
+export default ImcButton
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,17 +1,30 @@
 import SuscribeButton from "../components/SuscribeButton";
 import ConnexionButton from "../components/ConnexionButton";
+import ImcButton from "../components/ImcButton";
 import "./Welcome.css"
 
 const Welcome = ()=>{
     return(
-        <div>  
+        <div> 
+          <div className="imcButtonStyle">
+   <ImcButton/>
+   </div> 
           <div className = "saladePicture">
             {/* image avec salade */}
           </div>
-
-          <div>
+          <div className="containerButton">
+    <ConnexionButton/>
      <SuscribeButton/>
-     <ConnexionButton/>
+     </div>
+     <p className = "nboomText">Avec NutriBoom, boostez votre hygiène de vie !</p>
+     <div className="containerText">
+<p className="description"><strong> Suivi Journalier</strong>
+<br /> Complétez votre parcours santé en ajoutant vos aliments et exercices préférés à votre journal.
+<br /> <strong>Tableau de bord</strong>
+<br />Obtenez un rapport sur une durée grâce à notre tableau de bord.
+<br /><strong>Alimentez la base de données de la communauté</strong>
+<br />Soumettez vos aliments/exercices afin de nous permettre d'améliorervotre expérience utilisateur
+</p>
      </div>
      
       </div>
