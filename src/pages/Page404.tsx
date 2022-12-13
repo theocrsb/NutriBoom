2ce49f35eb8e700fcce49f9edb5f1549a8d8cb90
OZIER Ludovic (SNCF VOYAGEURS / TECHNICENTRE DE PARIS NORD / EQUIPE EXAMENS JOUR) <9001074b@commun.ad.sncf.fr>
2022-12-13 09:54:28 +0100

    mise en commmun

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -4,7 +4,7 @@ const Page404 = () => {
     return (
         <div>
             <img src={process.env.PUBLIC_URL + `./assets/carotte triste.png`} alt="carotte triste" className = "carotteLogoTriste" />
-            <h1 className ="text404"> BadaBOOM... </h1>
+            <h1 className ="text404"> BadaBOOM...</h1>
             <h2 className = "text404">La page que vous cherchez est introuvable </h2>
             <Link className="text404" to="/">Retour à la page d'accueil</Link>
         </div>
