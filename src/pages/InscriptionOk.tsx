1319879c646391322a063f7208785b58972757ae
Theocrsb <theo_crosbie@yahoo.fr>
2023-01-03 13:46:39 +0100

    ajout button connect apres inscripion

--- src/pages/InscriptionOk.tsx
+++ src/pages/InscriptionOk.tsx
@@ -1,18 +1,29 @@
-import {Link} from "react-router-dom"
-import "./InscriptionOk.css"
-const InscriptionOk =()=>{
-    return(
-        <div>
-        <div className="textAccueil">
-        <h1 className = "textInscription"> Boom...</h1>
-        <h1 className="textInscription">Tu es enfin inscrit sur la meilleure app de suivi calorique</h1>
-        </div>
-        <p className ="textInscriptionBis">Connecte toi dès maintenant pour pouvoir profiter de notre offre ! </p>
-        <Link to ="/connexion" className="link">
+import { Link } from 'react-router-dom';
+import ConnexionButton from '../components/ConnexionButton';
+import './InscriptionOk.css';
+import './Welcome.css';
+
+const InscriptionOk = () => {
+  return (
+    <div>
+      <div className='textAccueil'>
+        <h1 className='textInscription'> Boom...</h1>
+        <h1 className='textInscription'>
+          Tu es enfin inscrit sur la meilleure app de suivi calorique
+        </h1>
+      </div>
+      <p className='textInscriptionBis'>
+        Connecte toi dès maintenant pour pouvoir profiter de notre offre !{' '}
+      </p>
+      {/* <Link to ="/connexion" className="link">
         <p className ="lien">Se connecter</p>
+        </Link> */}
+      <div className='containerButton'>
+        <Link to='/connexion'>
+          <ConnexionButton />
         </Link>
-        
-        </div>
-    )
-}
-export default InscriptionOk
+      </div>
+    </div>
+  );
+};
+export default InscriptionOk;
