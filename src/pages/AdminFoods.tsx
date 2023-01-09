0c5da3c7b8139c71046ea9e7a56bd0596bf38263
Mykhado <mikawestindies@gmail.com>
2023-01-09 17:02:15 +0100

    resolution bug state ( pb uuid)

--- src/pages/AdminFoods.tsx
+++ src/pages/AdminFoods.tsx
@@ -1,79 +1,21 @@
 import React from "react";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
 import { Food } from "./Main";
-import { User } from "./Main";
-import { UserRole } from "./Main";
 import "./Admin.css";
 import { useNavigate } from "react-router-dom";
 import { FcCheckmark } from "react-icons/fc";
-let allFoods: Food[] = [];
-let convertValue: boolean;
+
+// let convertValue: boolean;
 const AdminFoods = () => {
   // Ajout du navigate
   const navigate = useNavigate();
   const [updateModerate, setUpdateModerate] = useState<string>();
-  const [optionState, setOptionState] = useState<string>("true");
-  // const updateModerate = useRef<HTMLSelectElement>(null);
   const [validateState, setValidateState] = useState<string>();
   const [mesFoods, setMesFoods] = useState<Food[]>([]);
 
-  // const booleanFunction = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  //   setValidateState(e.currentTarget.value);
-  // };
-
-  const handleCheck = (e: React.MouseEvent<HTMLInputElement>) => {
-    console.log(e.currentTarget.value);
-    setValidateState(e.currentTarget.value);
-  };
-
-  const ModerateFunction = (e: React.SyntheticEvent<HTMLSelectElement>) => {
-    // let valeurConvertieNombre = Number(e.currentTarget.value);
-
-    if (e.currentTarget.value) {
-      if (e.currentTarget.value === "true") {
-        convertValue = true;
-      } else if (e.currentTarget.value === "false") {
-        convertValue = false;
-      }
-    }
-    console.log("valeur de convert value avant set update", convertValue);
-    console.log("valeur de update moderate avant set update", updateModerate);
-    // setUpdateModerate(convertValue);
-    setOptionState(e.currentTarget.value);
-  };
-  // useEffect(() => {
-  //   setUpdateModerate(convertValue);
-  //   console.log("valeur dans le useeffect de  update moderate", updateModerate);
-  // }, [optionState]);
-
-  // let convertValue: boolean;
-
-  // let valueSelect = updateModerate.current?.value;
-  // console.log("value", valueSelect);
-
-  // if (value) {
-  //   if (value === "true") {
-  //     convertValue = true;
-  //   } else if (value === "false") {
-  //     convertValue = false;
-  //   }
-  // }
-  // console.log("valeur converti en boolean", convertValue);
-
-  // console.log("update Moderate valeur", updateModerate.current?.value);
-  const fonctionTest = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(
-      "voici le resultat du test valeur du state update moderate",
-      updateModerate
-    );
-    console.log("valeur converti en boolean", convertValue);
-    // console.log("value dans le update", valueSelect);
-  };
-
-  const getFoods = async () => {
-    await axios
+  useEffect(() => {
+    axios
       .get("http://localhost:8080/api/foods", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("accesstoken")}`,
@@ -81,37 +23,36 @@ const AdminFoods = () => {
       })
       .then((res) => {
         console.log("mes foods", res.data);
-        allFoods = res.data;
         setMesFoods(res.data);
-        console.log("mes  dansfoods le state", mesFoods);
+        console.log("mes  aliments dans mesfoods ", mesFoods);
       })
       .catch((error) => {
         console.log("something went wrong", error);
       });
-  };
-  useEffect(() => {
-    getFoods();
-    // let response = await axios
-    //     .get("http://localhost:8080/api/foods", {
-    //       headers: {
-    //         Authorization: `Bearer ${localStorage.getItem("accesstoken")}`,
-    //       },
-    //     })
-    //     .then((res) => {
-    //       console.log("mes foods", res.data);
-    //       allFoods = res.data;
-    //       setMesFoods(res.data);
-    //       console.log("mes  dansfoods le state", mesFoods);
-    //     })
-    //     .catch((error) => {
-    //       console.log("something went wrong", error);
-    //     });
   }, []);
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log("update moderate", updateModerate);
-    setOptionState(e.currentTarget.value);
-
-    // console.log("what is this ? ", e.target);
+  const ModerateFunction = (e: React.SyntheticEvent<HTMLSelectElement>) => {
+    // if (e.currentTarget.value) {
+    //   if (e.currentTarget.value === "true") {
+    //     convertValue = true;
+    //   } else if (e.currentTarget.value === "false") {
+    //     convertValue = false;
+    //   }
+    // }
+    console.log(
+      "value moderate fonction -------------------",
+      e.currentTarget.value
+    );
+    setUpdateModerate(e.currentTarget.value);
+  };
+  const fonctionTest = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(
+      "voici le resultat du test valeur du state update moderate",
+      updateModerate
+    );
+  };
+  const handleCheck = (e: React.MouseEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    setValidateState(e.currentTarget.value);
   };
 
   const handleDeleteli = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -146,7 +87,7 @@ const AdminFoods = () => {
       .patch(
         `http://localhost:8080/api/foods/${e.currentTarget.value}`,
         {
-          validate: convertValue,
+          // validate: convertValue,
         },
         {
           headers: {
@@ -176,34 +117,17 @@ const AdminFoods = () => {
       });
   };
 
-  let mesFoodsFilter = [];
-  if (mesFoods) {
-    for (let i = 0; i < mesFoods.length; i++) {
-      if (mesFoods[i].validate.toString() === validateState) {
-        mesFoodsFilter.push(mesFoods[i]);
-      }
-    }
-  }
+  // let mesFoodsFilter = [];
+  // if (mesFoods) {
+  //   for (let i = 0; i < mesFoods.length; i++) {
+  //     if (mesFoods[i].validate.toString() === validateState) {
+  //       mesFoodsFilter.push(mesFoods[i]);
+  //     }
+  //   }
+  // }
 
   return (
     <div className="container-food">
-      {/* <select
-        name="food"
-        id="foodAdmin"
-        className="htmlForm-label select"
-        // value={weightState}
-        onChange={booleanFunction}
-      >
-        <option key={uuidv4()} value="">
-          Sélectionner une valeur
-        </option>
-        <option key={uuidv4()} value="true">
-          true
-        </option>
-        <option key={uuidv4()} value="false">
-          false
-        </option>
-      </select> */}
       <div className="triFood">
         <h2>Selectionne les aliments a afficher</h2>
         <br />
@@ -259,32 +183,24 @@ const AdminFoods = () => {
               .filter((food) =>
                 food.validate.toString().includes(validateState)
               )
-              .map((foodfiltered) => (
-                <li key={uuidv4()}>
-                  {foodfiltered.name}
-
+              .map((foodfiltered, i) => (
+                <li key={i}>
+                  {foodfiltered.name}/{foodfiltered.validate.toString()}
                   <select
                     name="food"
                     id="foodAdmin"
                     className="htmlForm-label select"
-                    // defaultValue={foodfiltered.validate.toString()}
-                    onChange={(e) => {
-                      const selectedModerate = e.currentTarget.value;
-                      setUpdateModerate(selectedModerate);
-                      console.log("voici selected moderate", selectedModerate);
-                    }}
-                    // ref={updateModerate}
-                    // onChange={handleChange}
+                    defaultValue={foodfiltered.validate.toString()}
+                    onChange={ModerateFunction}
                   >
-                    <option key={uuidv4()} value="true">
+                    <option key={i + 1} value="true">
                       Affiché
                     </option>
 
-                    <option key={uuidv4()} value="false">
+                    <option key={i + 2} value="false">
                       Masqué
                     </option>
                   </select>
-
                   <button
                     className="buttonDeleteAliment"
                     onClick={handleDeleteli}
@@ -295,36 +211,30 @@ const AdminFoods = () => {
                   <button style={{ color: "red" }} onClick={fonctionTest}>
                     test update
                   </button>
-
                   <button value={foodfiltered.id} onClick={updateFunction}>
                     <FcCheckmark />
                   </button>
                 </li>
               ))
-          : mesFoods.map((food) => (
-              <li key={uuidv4()}>
+          : mesFoods.map((food, i) => (
+              <li key={i}>
                 {food.name}
                 <select
                   name="food"
                   id="foodAdmin"
                   className="htmlForm-label select"
-                  // defaultValue={food.validate.toString()}
-                  // onChange={handleChange}
-                  onChange={(e) => {
-                    const selectedModerate = e.currentTarget.value;
-                    setUpdateModerate(selectedModerate);
-                    console.log("voici selected moderate", selectedModerate);
-                  }}
-                  // ref={updateModerate}
+                  defaultValue={food.validate.toString()}
+                  onChange={ModerateFunction}
                 >
-                  <option key={uuidv4()} value="true">
+                  <option key={i + 1} value="true">
                     Affiché
                   </option>
 
-                  <option key={uuidv4()} value="false">
+                  <option key={i + 2} value="false">
                     Masqué
                   </option>
                 </select>
+
                 <button
                   className="buttonDeleteAliment"
                   onClick={handleDeleteli}
