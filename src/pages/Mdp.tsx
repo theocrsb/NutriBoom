40677e0e740a6c7eaa68e88421f4429ea6117929
Theocrsb <theo_crosbie@yahoo.fr>
2023-01-11 15:54:35 +0100

    debut gestion evnoie email

--- src/pages/Mdp.tsx
+++ src/pages/Mdp.tsx
@@ -1,10 +1,10 @@
-import "./Connexion.css";
-import { Link } from "react-router-dom";
-import ConnexionButton from "../components/ConnexionButton";
-import { useState, useEffect, FormEvent, useContext } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../contexts/Auth-context";
+import './Connexion.css';
+import { Link } from 'react-router-dom';
+import ConnexionButton from '../components/ConnexionButton';
+import { useState, useEffect, FormEvent, useContext } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../contexts/Auth-context';
 
 const Mdpx = () => {
   const { onAuthChange } = useContext(AuthContext);
@@ -19,35 +19,24 @@ const Mdpx = () => {
   };
   const handleLoginForm = async (e: FormEvent) => {
     e.preventDefault();
-    console.log("button form clicked");
+    console.log('button form clicked');
     console.log(mailState);
 
     await axios
-      .post("http://localhost:8080/api/auth/login", {
+      .post('http://localhost:8080/api/users/reset/password', {
         email: mailState,
       })
-      .then((token) => {
-        console.log(token.data.access_token);
-        const tokens = token.data.access_token;
-        localStorage.setItem("accesstoken", tokens);
-
-        // recuperation du token dans le local storage afin de l'utiliser dans les context d'authentification
-        // recupToken = localStorage.getItem("accesstoken");
-        onAuthChange(tokens);
-        // console.log("valeur de onAuthChange", onAuthChange("montoken"));
-        //console.log("token recupere dans le local storage", recupToken);
-        setTimeout(() => {
-          navigate("/main");
-        }, 1000);
-        setMessage("Regardez vos mails!");
+      .then((response) => {
+        console.log(response);
+        setMessage(response.data);
       })
       .catch((error) => {
-        console.log("connexion impossible", error);
+        console.log('connexion impossible', error);
         if (!mailState) {
-          console.log("erreur", error.response.data.message);
+          console.log('erreur', error.response.data.message);
           setMessage(error.response.data.message);
-        } else if (error.message === "Request failed with status code 401") {
-          setMessage(" adresse mail inconnu(e)");
+        } else if (error.message === 'Request failed with status code 401') {
+          setMessage(' adresse mail inconnu(e)');
         }
       });
   };
@@ -55,17 +44,17 @@ const Mdpx = () => {
   // useEffect pour tester les states car ils sont asynchrones//
   //et affichent avant re-render une première valeur undefined//
   useEffect(() => {
-    console.log("mail dans useEffect", mailState);
+    console.log('mail dans useEffect', mailState);
   });
 
   return (
-    <div className="fondCarotte">
-      <div className="textConnect">
+    <div className='fondCarotte'>
+      <div className='textConnect'>
         <h1>Tu as oublié ton mot de passe?</h1>
 
-        <Link className="linkSub" to="/suscribe">
-          <p className="lienInscription">
-            {" "}
+        <Link className='linkSub' to='/suscribe'>
+          <p className='lienInscription'>
+            {' '}
             <strong>Entres ton email </strong>
             <p>
               Tu recevras dans ta boite de reception un lien pour te crée un
@@ -75,26 +64,26 @@ const Mdpx = () => {
         </Link>
       </div>
       <div>
-        <form className="formConnexion" onSubmit={handleLoginForm}>
-          <div className="mb-3">
-            <label htmlFor="inputMail" className="htmlForm-label" />
+        <form className='formConnexion' onSubmit={handleLoginForm}>
+          <div className='mb-3'>
+            <label htmlFor='inputMail' className='htmlForm-label' />
             <input
-              type="mail"
-              className="htmlForm-control"
-              id="inputMail"
-              placeholder="mail"
+              type='mail'
+              className='htmlForm-control'
+              id='inputMail'
+              placeholder='mail'
               onInput={mailFunction}
             />
           </div>
 
-          <button type="submit" className="btn inscription">
-            {" "}
+          <button type='submit' className='btn inscription'>
+            {' '}
             Envoyer
           </button>
         </form>
       </div>
-      <span className="message">{message}</span>
-      <div className="connexionButton">{/* <ConnexionButton /> */}</div>
+      <span className='message'>{message}</span>
+      <div className='connexionButton'>{/* <ConnexionButton /> */}</div>
     </div>
   );
 };
