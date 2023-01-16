86d4eb7a443f9505e81ee66e677455adedc7e4f0
Mykhado <mikawestindies@gmail.com>
2023-01-16 09:44:43 +0100

    push design css

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom";
 import "./Page404.css";
 const Page404 = () => {
-    return (
-        <div>
-            <img src={process.env.PUBLIC_URL + `./assets/carotte triste.png`} alt="carotte triste" className = "carotteLogoTriste" />
-            <h1 className ="text404"> BadaBOOM... </h1>
-            <h2 className = "text404">La page que vous cherchez est introuvable </h2>
-            <Link className="text404" to="/">Retour à la page d'accueil</Link>
+  return (
+    <div className="page-404">
+      <section className="container-404">
+        <div className="img-end">
+          <img
+            src={process.env.PUBLIC_URL + `./assets/carotteTriste.png`}
+            alt="carotte triste"
+            className="carotteLogoTriste"
+          />
         </div>
-    )
-}
+        <div className=" text-404">
+          <h1 className="text404"> BadaBOOM... </h1>
+          <h2 className="text404">
+            La page que vous cherchez est introuvable{" "}
+          </h2>
+          <Link className="text404" to="/">
+            Retour à la page d'accueil
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Page404;
