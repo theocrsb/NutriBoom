2565ddf0f9dd275dd0828b7e3740cacd2211ec36
Mykhado <mikawestindies@gmail.com>
2023-01-16 14:09:09 +0100

    css page tuto

--- src/pages/Tuto.tsx
+++ src/pages/Tuto.tsx
@@ -3,22 +3,21 @@ import "./Tuto.css";
 
 const Tuto = () => {
   return (
-    <div>
-      <h1>Comment utiliser NutriBoom</h1>
-      <div className="video">
-        <iframe
-          width="840"
-          height="472.5"
-          src="https://www.youtube.com/embed/jIkOiEaSfHI"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+    <div className="tuto-page">
+      <div className="container-tuto">
+        <h1 className="title-tuto">Comment utiliser NutriBoom</h1>
+        <div className="video">
+          <iframe
+            width="840"
+            height="472.5"
+            src="https://www.youtube.com/embed/jIkOiEaSfHI"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
-      {/* <Link className="text404" to="/">
-        Retour à la page d'accueil
-      </Link> */}
     </div>
   );
 };
