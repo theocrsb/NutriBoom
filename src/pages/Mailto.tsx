475e852b7fb76ba9f52a5619a5c13f5c645c11bd
Mykhado <mikawestindies@gmail.com>
2023-01-16 11:16:39 +0100

    changement de chemin mot de passe oublié et un peu de css

--- src/pages/Mailto.tsx
+++ src/pages/Mailto.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { FormEvent, useRef, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
+import "./Mailto.css";
 
 const Mailto = () => {
   const emailElement = useRef<HTMLInputElement>(null);
   const nomElement = useRef<HTMLInputElement>(null);
   const texteAreaElement = useRef<HTMLInputElement>(null);
-  const navigate =useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmitForm = (e: React.FormEvent) => {
     e.preventDefault();
@@ -27,7 +28,7 @@ const Mailto = () => {
 
       .then((response) => {
         console.log("le console.log du response.data", response.data);
-        navigate('/welcome')
+        navigate("/welcome");
       })
 
       .catch((error) => {
@@ -36,41 +37,44 @@ const Mailto = () => {
   };
 
   return (
-    <div className="App">
+    <div className="contact-page">
       <h1>Faites nous part de vos suggestions</h1>
       {/*<form onSubmit={(e) =>{handleSubmitForm(e)}}>*/}
       <form className="w-50 m-auto" onSubmit={handleSubmitForm}>
         <div className="form-floating mb-3">
           <input
             type="text"
-            className="form-control"
+            className="form-control contact-us"
             id="nomUser"
-            placeholder="Votre nom"
             ref={nomElement}
           />
-          <label htmlFor="nomUser">Nom</label>
+          <label htmlFor="nomUser" className="label-contact">
+            Nom
+          </label>
         </div>
 
         <div className="form-floating mb-3">
           <input
             type="email"
-            className="form-control"
+            className="form-control contact-us"
             id="emailUser"
-            placeholder="name@example.com"
             ref={emailElement}
           />
-          <label htmlFor="emailUser">Email</label>
+          <label htmlFor="emailUser" className="label-contact">
+            Email
+          </label>
         </div>
 
         <div className="form-floating mb-3">
           <input
             type="textarea"
-            className="form-control"
-            id="nomUser"
-            placeholder="Votre text"
+            className="form-control contact-us"
+            id="textUser"
             ref={texteAreaElement}
           />
-          <label htmlFor="nomUser">text</label>
+          <label htmlFor="nomUser" className="label-contact">
+            Text
+          </label>
         </div>
 
         <button className="mt-3 btn btn-primary mb-3" type="submit">
