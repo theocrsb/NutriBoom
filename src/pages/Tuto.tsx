42e360952b97fc7be92b45d2984d37202027f285
PPRonee <100339149+PPRonee@users.noreply.github.com>
2023-01-17 15:51:46 +0100

    new vid

--- src/pages/Tuto.tsx
+++ src/pages/Tuto.tsx
@@ -8,9 +8,9 @@ const Tuto = () => {
         <h1 className="title-tuto">Comment utiliser NutriBoom</h1>
         <div className="video">
           <iframe
-            width="840"
-            height="472.5"
-            src="https://www.youtube.com/embed/jIkOiEaSfHI"
+            width="1008"
+            height="567"
+            src="https://www.youtube.com/embed/vxiaHWhHMjU"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
