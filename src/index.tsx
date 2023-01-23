d7efd59989ac8792ecf067dd8e4b4cd54cc1869a
Theocrsb <theo_crosbie@yahoo.fr>
2023-01-23 11:31:27 +0100

    test 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./index.css";
-import App from "./App";
-import { AuthContextProvider } from "./contexts/Auth-context";
-import { UserContextProvider } from "./contexts/User-Context";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
+import App from './App';
+import { AuthContextProvider } from './contexts/Auth-context';
+import { UserContextProvider } from './contexts/User-Context';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   // Import et mise en place des differents context qui engloberont toute notre APP
@@ -20,4 +20,5 @@ root.render(
     </AuthContextProvider>
   </React.StrictMode>
   //Test 1 deploy
+  // 2
 );
